26f9b6b48ff0eb05e7e2aa27c27dd58e459a2567
Austin Heil <austinmh12@gmail.com>
2023-12-18 19:27:21 -0500

    wasm update

--- vamp_clone.js
+++ vamp_clone.js
@@ -1865,7 +1865,7 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 496, __wbg_adapter_34);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper41539 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper41554 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 496, __wbg_adapter_37);
         return addHeapObject(ret);
     };
